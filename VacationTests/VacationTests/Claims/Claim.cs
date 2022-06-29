e53712e27ef4dd5a7e56fd1616c6d231fc983465
s.ashurov <s.ashurov@skbkontur.ru>
2022-06-29 22:09:14 +0500

    Сделал Задание №6

--- VacationTests/VacationTests/Claims/Claim.cs
+++ VacationTests/VacationTests/Claims/Claim.cs
@@ -5,68 +5,103 @@ using Newtonsoft.Json.Converters;
 
 namespace VacationTests.Claims
 {
-    public class Claim
-    {
-        // метод инициализации
-        public Claim(string id, ClaimType type, ClaimStatus status, Director director, DateTime startDate,
-            DateTime endDate, int? childAgeInMonths, string userId, bool paidNow)
-        {
-            Id = id;
-            Type = type;
-            Status = status;
-            Director = director;
-            StartDate = startDate;
-            EndDate = endDate;
-            ChildAgeInMonths = childAgeInMonths;
-            UserId = userId;
-            PaidNow = paidNow;
-        }
-        // свойства класса
-        public string Id { get; }
+    public record Claim(
+        // перечисляем какие свойства будут у класса Claim
+        string Id,
         [property: JsonConverter(typeof(StringEnumConverter))]
-        public ClaimType Type { get; }
-        public ClaimStatus Status { get; }
-        public Director Director { get; }
-        public DateTime StartDate { get; }
-        public DateTime EndDate { get; }
-        public int? ChildAgeInMonths { get; }
-        public string UserId { get; }
-        public bool PaidNow { get; }
+        ClaimType Type,
+        ClaimStatus Status,
+        Director Director,
+        DateTime StartDate,
+        DateTime EndDate,
+        int? ChildAgeInMonths,
+        string UserId,
+        bool PaidNow)
+    {
+        // добавляем статический метод для создания экземпляра класса со значениями по умолчанию
+        public static Claim CreateDefault()
+        {
+            var random = new Random();
+            var randomClaimId = random.Next(1, 101).ToString();
+
+            return new Claim(
+                randomClaimId,
+                ClaimType.Paid,
+                ClaimStatus.NonHandled,
+                Director.CreateDefault(),
+                // DirectorBuilder.ADefaultDirector(),
+                // new Director(1, "Name", "Position"),
+                DateTime.Now.AddDays(7).Date,
+                DateTime.Now.AddDays(12).Date,
+                null,
+                "1",
+                false);
+        }
+
+        // можно также добавить второй метод для создания типичного заявления по уходу за ребёнком
+        public static Claim CreateChildType()
+        {
+            var random = new Random();
+            var childAgeInMonths = random.Next(1, 101);
+            return CreateDefault() with
+            {
+                Type = ClaimType.Child,
+                ChildAgeInMonths = childAgeInMonths
+            };
+        }
     }
 
-    public class Director
+    public record Director(
+        int Id,
+        string Name,
+        string Position)
     {
-        public Director(int id, string name, string position)
+        public static Director CreateDefault()
         {
-            Id = id;
-            Name = name;
-            Position = position;
+            return new Director(
+                14,
+                "Бублик Владимир Кузьмич",
+                "Директор департамента");
         }
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Position { get; set; }
+
+        public static Director CreateSuperDirector()
+        {
+            return CreateDefault() with
+            {
+                Id = 24320,
+                Name = "Кирпичников Алексей Николаевич",
+                Position = "Руководитель управления"
+            };
+        }
     }
-    
+
     // Об enum https://ulearn.me/course/basicprogramming/Konstanty_i_enum_y_f1740706-b8e2-4bd4-ab87-3cc710a52449
     public enum ClaimType
     {
         [System.ComponentModel.Description("Дополнительный")] [EnumMember(Value = "Дополнительный")]
         Additional,
+
         [System.ComponentModel.Description("По уходу за ребенком")] [EnumMember(Value = "По уходу за ребенком")]
         Child,
+
         [System.ComponentModel.Description("Не оплачиваемый")] [EnumMember(Value = "Не оплачиваемый")]
         NotPaid,
+
         [System.ComponentModel.Description("Основной")] [EnumMember(Value = "Основной")]
         Paid,
+
         [System.ComponentModel.Description("На учебу")] [EnumMember(Value = "На учебу")]
         Study
     }
+
     public enum ClaimStatus
     {
         [System.ComponentModel.Description("Согласовано")]
         Accepted = 0,
+
         [System.ComponentModel.Description("Отклонено")]
         Rejected = 1,
+
         [System.ComponentModel.Description("На согласовании")]
         NonHandled = 2
     }
--- VacationTests/VacationTests/Claims/ClaimTests.cs
+++ VacationTests/VacationTests/Claims/ClaimTests.cs
@@ -213,36 +213,37 @@ namespace VacationTests.Claims
             ClaimStorage.Add(claims);
         }
         
-        // [Test]
-        // public void ClaimTest5Record()
-        // {
-        //     var page = Navigation.OpenEmployeeVacationList(employeeId1);
-        //     
-        //     // создаем 3 заявления на отпуск
-        //     // CreateDefault() статический метод класса, поэтому его можно вызвать так - Claim.CreateDefault()
-        //     // для указания нужных свойств используется ключевое слово `with`
-        //     // в фигурных скобках перечисляются необходимые для теста свойства и их значения
-        //     var claim1 = Claim.CreateDefault() with {Id = "1", UserId = employeeId1};
-        //     var claim2 = Claim.CreateDefault() with {Id = "2", UserId = employeeId1};
-        //     var claim3 = Claim.CreateDefault() with {Id = "3", UserId = employeeId2};
-        //
-        //     // записываем заявления в базу
-        //     ClaimStorage.Add(new[] {claim1, claim2, claim3});
-        //
-        //     page.Refresh();
-        //
-        //     // проверяем, что на списке сотрудника с id=1 два заявления с номерами 1 и 2
-        //     page.ClaimList.Items.Select(x => x.TitleLink.Text)
-        //         .Wait().EqualTo(new[] {"Заявление 1", "Заявление 2"});
-        // }
-        //
-        // [Test]
-        // public void ClaimTest6Record()
-        // {
-        //     // создаем 3 заявления на отпуск
-        //     // записываем заявления в базу
-        //     ClaimStorage.Add(new[] {Claim.CreateDefault(), Claim.CreateDefault(), Claim.CreateChildType()});
-        // }
+        [Test]
+        public void ClaimTest5Record()
+        {
+            var page = Navigation.OpenEmployeeVacationList(employeeId1);
+            
+            // создаем 3 заявления на отпуск
+            // CreateDefault() статический метод класса, поэтому его можно вызвать так - Claim.CreateDefault()
+            // для указания нужных свойств используется ключевое слово `with`
+            // в фигурных скобках перечисляются необходимые для теста свойства и их значения
+            var claim1 = Claim.CreateDefault() with {Id = "1", UserId = employeeId1};
+            var claim2 = Claim.CreateDefault() with {Id = "2", UserId = employeeId1};
+            var claim3 = Claim.CreateDefault() with {Id = "3", UserId = employeeId2};
+        
+            // записываем заявления в базу
+            ClaimStorage.Add(new[] {claim1, claim2, claim3});
+        
+            page.Refresh();
+        
+            // проверяем, что на списке сотрудника с id=1 два заявления с номерами 1 и 2
+            page.ClaimList.Items.Select(x => x.TitleLink.Text)
+                .Wait().EqualTo(new[] {"Заявление 1", "Заявление 2"});
+        }
+        
+        [Test]
+        public void ClaimTest6Record()
+        {
+            Navigation.OpenEmployeeVacationList(employeeId1);
+            // создаем 3 заявления на отпуск
+            // записываем заявления в базу
+            ClaimStorage.Add(new[] {Claim.CreateDefault(), Claim.CreateDefault(), Claim.CreateChildType()});
+        }
         
         private string employeeId1 = "1";
         private string employeeId2 = "2";
